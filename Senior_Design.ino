8daa94b548de501c5347b57e4dfbf07d50626071
Henry V. Gilbert <henrygilbert609@gmail.com>
2019-03-29 22:57:58 -0400

    Add files via upload

--- Senior_Design.ino
+++ Senior_Design.ino
@@ -4,63 +4,74 @@
  * ==========================================================================
  * ==========================================================================
  */ 
+
+
 // Liquidcrystal.h  defines the LCD functions and classes.
 // max6675.h defines the K-type thermocouple c
-
 #include <LiquidCrystal.h>
 #include <max6675.h>
 
- /* Goals: Acquire 2 sets of 6 temperatures from refrigerants.
- *        Allow manual pressure input using knobs (pots or rotary) and a button. 
- *        Display interactive menu on LCD
- *        Calculate performance data on the LCD screen. 
- *        Switch to alternate between scroll compressor and reciprocating. 
- *        Wire an LED matrix of  lights to show which measurements are being taken. 
- *        INPUTS: Thermocouple temperatures, manual pressures, knobs for tuning digits, buttons for P. 
- *        OUTPUTS: LCD performance, LED matrix, 
- */
+// Thermocouple declarations for pins. 
+const int ktc1SO = 22, ktc1CS = 24, ktc1CLK = 26;
+const int ktc2SO = 23, ktc2CS = 25, ktc2CLK = 27;  
+const int ktc3SO = 28, ktc3CS = 30, ktc3CLK = 32;  
+const int ktc4SO = 29, ktc4CS = 31, ktc4CLK = 33;  
+const int ktc5SO = 34, ktc5CS = 36, ktc5CLK = 38;  
+const int ktc6SO = 35, ktc6CS = 37, ktc6CLK = 39;  
+const int ktc7SO = 40, ktc7CS = 42, ktc7CLK = 44;
+const int ktc8SO = 41, ktc8CS = 43, ktc8CLK = 45;
 
-int ktc1SO = 30, ktc1CS = 31, ktc1CLK = 32;  // Declaration of thermocouple pins 
-int ktc2SO = 33, ktc2CS = 34, ktc2CLK = 35;  // Declaration of thermocouple pins 
-int ktc3SO = 36, ktc3CS = 37, ktc3CLK = 38;  // Declaration of thermocouple pins 
-int ktc4SO = 39, ktc4CS = 40, ktc4CLK = 41;  // Declaration of thermocouple pins 
-int ktc5SO = 42, ktc5CS = 43, ktc5CLK = 44;  // Declaration of thermocouple pins 
-int ktc6SO = 45, ktc6CS = 46, ktc6CLK = 47;  // Declaration of thermocouple pins 
+// LCD pinmode declaration. Bottom 3 set of pins on MEGA. 
+const int rs = 48, en = 49, d4 = 50, d5 = 51, d6 = 52, d7 = 53;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);   // Object declaration of LCD  
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; // Declaration of LCD pins 
-LiquidCrystal lcd1(rs, en, d4, d5, d6, d7);   // Object declaration of LCD  
-MAX6675 ktc1(ktc1CLK, ktc1CS, ktc1SO);    // Thermocouple declaration
-MAX6675 ktc2(ktc2CLK, ktc2CS, ktc2SO);    // Thermocouple declaration
-MAX6675 ktc3(ktc3CLK, ktc3CS, ktc3SO);    // Thermocouple declaration
-MAX6675 ktc4(ktc4CLK, ktc4CS, ktc4SO);    // Thermocouple declaration
-MAX6675 ktc5(ktc5CLK, ktc5CS, ktc5SO);    // Thermocouple declaration
-MAX6675 ktc6(ktc6CLK, ktc6CS, ktc6SO);    // Thermocouple declaration
+// Declarations of thermocouples 1-8. 7-8 are optional. 
+MAX6675 ktc1(ktc1CLK, ktc1CS, ktc1SO);
+MAX6675 ktc2(ktc2CLK, ktc2CS, ktc2SO);    
+MAX6675 ktc3(ktc3CLK, ktc3CS, ktc3SO);   
+MAX6675 ktc4(ktc4CLK, ktc4CS, ktc4SO);    
+MAX6675 ktc5(ktc5CLK, ktc5CS, ktc5SO);    
+MAX6675 ktc6(ktc6CLK, ktc6CS, ktc6SO);    
+MAX6675 ktc7(ktc7CLK, ktc7CS, ktc7SO); 
+MAX6675 ktc8(ktc8CLK, ktc8CS, ktc8SO); 
 
-// Initializations for the LEDs. (Might change this later and use diagram instead). 
-short int led_row1 = 2, led_row2 = 3, led_row3 = 4, led_row4 = 5;
-short int led_col1 = 6, led_col2 = 7, led_col3 = 8, led_col4 = 9;
+// LED pins for pressure 
+const int high_pressure_led = 2;
+const int low_pressure_led = 3;
+
+// LED pins for refrigerant. 
+const int compressor_out = 4;
+const int condenser_out = 5;
+const int throttle_out = 6;
+const int evaporator_out = 7;
+
+// LED pins for performance
+const int mass_flow_led = 8;
+const int efficiency_led = 9; 
+
 // Buttons for high pressure and low pressure entry. 
-short int high_pressure_button = 10, low_pressure_button = 11;
+short int high_pressure_button = 46, low_pressure_button = 47;
 
-// Main resistance knob input. Must go to analog input. 
+// Variables for the interface. 
 int main_resistance; 
 int high_side_pressure;
 int low_side_pressure;
+
+// Derived values for the cycle. 
+double mass_flow;
+double efficiency;
  
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  lcd.begin(16,2);
+  lcd.begin(16,4);
   lcd.clear();
-  // Setup for LED Matrix:
-  pinMode(led_row1, OUTPUT);
-  pinMode(led_row2, OUTPUT);
-  pinMode(led_row3, OUTPUT);
-  pinMode(led_row4, OUTPUT);
-  pinMode(led_col1, OUTPUT);
-  pinMode(led_col2, OUTPUT);
-  pinMode(led_col3, OUTPUT);
-  pinMode(led_col4, OUTPUT);
+  pinMode(high_pressure_led, OUTPUT);
+  pinMode(low_pressure_led, OUTPUT);
+  pinMode(compressor_out, OUTPUT);
+  pinMode(condenser_out, OUTPUT);
+  pinMode(throttle_out, OUTPUT);
+  pinMode(evaporator_out, OUTPUT);
   pinMode(high_pressure_button, INPUT);
   pinMode(low_pressure_button, INPUT);
 }
@@ -68,23 +79,38 @@ void setup() {
 void loop() {
   main_resistance = analogRead(A0);
   // ktc.readCelsius(); reads celsius temperature. will be ktcx for different thermocouples 
+
+  
+  
   
   switch (switch_knob(main_resistance)) {
     case 1:
+      // read compressor outlet, print compressor outlet thermocouple. 
       break;
     case 2:
+      // Read high side from input: Possible interface? If not exists, prompt input. 
       break;
     case 3: 
+          // Read condenser outlet, print condenser outlet thermocouple. 
       break;
     case 4:
+          // Read throttle outlet, print throttle outlet thermocouple. 
       break;
     case 5: 
+          // Read low side pressure outlet, flash LEDS, print to LCD. 
       break;
     case 6:
+          // Read evaporator air outlet, print evaporator outlet thermocouple. 
       break;
     case 7: 
+        // Display refrigeration efficiency to LCD. Flash central LED. 
       break;
     case 8:
+        // Display refrigeration mass flow rate to LCD, flash central LED. 
       break;  
+    case 9;
+    break;
+    case 10:
+    break;
   } 
 }
--- a_functions.ino
+++ a_functions.ino
@@ -1,10 +1,3 @@
-int enter_high_pressure() {
-  int high_pressure = 0; //Read the value from the knob.
-  return high_pressure;
-}
-
-
-
 // Performance: gets all temperatures and pressures and calculates performance. 
 
 // Conditional if statements to allow for selecting measurements with potentiometer. 
@@ -19,31 +12,25 @@ int switch_knob(int main_resistance) {
     else if (main_resistance < 700) return 7;
     else if (main_resistance < 800) return 8;
     else if (main_resistance < 900) return 9;
+    else if (main_resistance < 1000) return 10;
     else return 0;
 }
 
 int resistance_to_integer(int main_resistance) {
   // calculate slope values and relationship between expected P 
-  int integer = 0;
-  return integer;
+  return 0;
 }
 
-void boolButton(int button1) {
-  if (button1 == true) {
-  } return 
+int menu_resistance_to_int(int main_resistance) {
+  // calcula
+  return 0;
 }
 
-void power_led(int input) {
-  // Change led write configuration based on integer input. 
-  // Possible create a void function to power off all leds. 
-  digitalWrite(led_row1, HIGH);
+int high_pressure_menu() {
+  
+  return 0;
 }
 
-void reset_led() {
-  digitalWrite(led_row1, LOW);
-  digitalWrite(led_row2, LOW);
-  digitalWrite(led_row3, LOW);
-  digitalWrite(led_row4, LOW);
-}
-
+int low_pressure_menu() {
+  return 0;
 }
