192f6429dfaf4fcf6de8cf300bc0dd6b2ea8802a
Henry V. Gilbert <henrygilbert609@gmail.com>
2019-04-15 18:42:44 -0700

    Add files via upload

--- a_functions.ino
+++ a_functions.ino
@@ -1,261 +1,235 @@
-// Performance: gets all temperatures and pressures and calculates performance. 
-// Conditional if statements to allow for selecting measurements with potentiometer. 
-// Directly enters the switch statement in the mainloop function. 
-
-// ============= DERIVED VALUES FROM DANIEL ==================
-
-
-double calculate_power(double tempEvap, double tempCond) {
-  double C[10] = {645.1275928, -2.295127832, -7.218567883, -0.005535838, 0.042861644,
-                    0.076252192, 0.000563255, -0.000610193, 0.000112464, -0.000130886};
-                    
-  return C[0] + C[1]*tempCond + C[2]*tempEvap + C[3]*pow(tempCond, 2) + C[4]*tempEvap*tempCond + 
-         C[5]*pow(tempEvap, 2) + C[6]*pow(tempCond, 3) + C[7]*tempEvap*pow(tempCond, 2) + C[8]*tempCond * pow(tempEvap, 2) +
-         C[8]*tempCond*pow(tempEvap, 2) + C[9]*pow(tempEvap, 3);
-}
-
-double calculate_mass_flow(double tempEvap, double tempCond) {
-  double C[10] = {40.15698882, 1.096371113, 0.14024549, 0.015865756, 0.002097207,
-                -0.001690066, 9.03205E-05, -2.70756E-05, -1.35767E-05, 5.15408E-06};
-                     
-  return C[0] + C[1]*tempCond + C[2]*tempEvap + C[3]*pow(tempCond, 2) + C[4]*tempEvap*tempCond + 
-         C[5]*pow(tempEvap, 2) + C[6]*pow(tempCond, 3) + C[7]*tempEvap*pow(tempCond, 2) + C[8]*tempCond * pow(tempEvap, 2) +
-         C[8]*tempCond*pow(tempEvap, 2) + C[9]*pow(tempEvap, 3);
-}
-
-double calculate_capacity(double tempEvap, double tempCond) {
-  double C[10] = {4433.99929, 107.9502108, -6.654767819, 2.002620595, -0.2210224213,
-               -0.1026412673, 0.002942954154, -0.00968481191, -0.0006961641807, 0.0002807495071};
-
-  return C[0] + C[1]*tempCond + C[2]*tempEvap + C[3]*pow(tempCond, 2) + C[4]*tempEvap*tempCond + 
-         C[5]*pow(tempEvap, 2) + C[6]*pow(tempCond, 3) + C[7]*tempEvap*pow(tempCond, 2) + C[8]*tempCond * pow(tempEvap, 2) +
-         C[8]*tempCond*pow(tempEvap, 2) + C[9]*pow(tempEvap, 3);
-}
-
-int calculate_efficiency(double power, double qin) {
-  // Efficiency = Qout/Win 
-  // mdot(h2-h1)/Win 
-  return 0;
-}
+// Performance: gets all temperatures and pressures and calculates performance.
+// Conditional if statements to allow for selecting measurements with potentiometer.
+// =====================================================================================
+// ORDER: MF, CAP, POWER, CURRENT. Recip 0 scr 1.
+// Coeffients for Reciprocating
 
 
-// ================ MENUS ==============
-void high_pressure_menu() {
-  while (digitalRead(high_pressure_switch) == HIGH) {
-    lcd.clear();
-    lcd.print("Enter HIGH P");
-    lcd.setCursor(0,2);
-    lcd.print(resistance_to_integer(analogRead(A0)));
-    delay(400);
+// Pointer tables for choosing the right coefficients in derived calculation.
+// To calculate, input the correct coeff based on switch value, and TE and TC.
+float calculate(float c[], float tc, float te) {
+    return  c[0] + c[1]*te + c[2]*tc + c[3]*pow(te, 2) + c[4]*te*tc +
+    c[5]*pow(tc, 2) + c[6]*pow(te, 3) + c[7]*tc*pow(te, 2) + c[8]*te*pow(tc, 2) + c[9]*pow(tc, 3);
   }
-  high_side_pressure = resistance_to_integer(analogRead(A0));
-}
+
+  // ================ MENUS ==============
+void high_pressure_menu() {
+    while (digitalRead(high_pressure_switch) == HIGH) {
+      lcd.clear();
+      lcd.print("Enter HIGH P");
+      lcd.setCursor(0, 2);
+      lcd.print(resistance_to_integer(analogRead(A0)));
+      delay(400);
+    }
+    high_side_pressure = resistance_to_integer(analogRead(A0));
+  }
 
 void low_pressure_menu() {
-  while (digitalRead(low_pressure_switch) == HIGH) {
+    while (digitalRead(low_pressure_switch) == HIGH) {
+      lcd.clear();
+      lcd.print("Enter LOW P");
+      lcd.setCursor(0, 2);
+      lcd.print(resistance_to_integer(analogRead(A0)));
+      delay(400);
+    }
+    low_side_pressure = resistance_to_integer(analogRead(A0));
+  }
+
+  // Convert resistance to integer values for pressure menus.
+  int resistance_to_integer(int resistance) {
+    return .68 * (resistance) + 100;
+  }
+
+  // Write the LCD with basic prompts.
+  // Accepts two inputs and always outputs the compressor mode as the first line on screen. 
+  void write_lcd(const char* prompt, int measurement) {
     lcd.clear();
-    lcd.print("Enter LOW P");
-    lcd.setCursor(0,2);
-    lcd.print(resistance_to_integer(analogRead(A0)));
-    delay(400);
-    } 
-  low_side_pressure = resistance_to_integer(analogRead(A0));
-}
+    lcd.setCursor(0, 1);
+    lcd.print(compressor_mode[digitalRead(comp_mode_switch)]);  // Reads the 0 or 1 value for switch to the compressor mode.
+    lcd.setCursor(0, 2);
+    lcd.print(prompt);
+    lcd.setCursor(0, 3);
+    lcd.print(measurement);
+  }
 
-// Convert resistance to integer values for pressure menus. 
-int resistance_to_integer(int resistance) {
-  return .68*(resistance) + 100;
-}
+  // ================= LCD WRITING AND CANCELLING ===============
+  void write_high_pressure_led() {
+    digitalWrite(high_pressure_led, HIGH);
+    digitalWrite(low_pressure_led, LOW);
+    digitalWrite(compressor_out, LOW);
+    digitalWrite(condenser_out, LOW);
+    digitalWrite(throttle_out, LOW);
+    digitalWrite(evaporator_out, LOW);
+    digitalWrite(mass_flow_led, LOW);
+    digitalWrite(capacitance_led, LOW);
+    digitalWrite(power_led, LOW);
+  }
 
-// Write the LCD with basic prompts. 
-void write_lcd(const char* prompt, int measurement) {
-  lcd.clear();
-  lcd.setCursor(0,1);
-  lcd.print(compressor_mode[digitalRead(comp_mode_switch)]);  // Reads the 0 or 1 value for switch to the compressor mode. 
-  lcd.setCursor(0,2);
-  lcd.print(prompt);
-  lcd.setCursor(0,3);
-  lcd.print(measurement);
-}
+  void write_low_pressure_led() {
+    digitalWrite(high_pressure_led, LOW);
+    digitalWrite(low_pressure_led, HIGH);
+    digitalWrite(compressor_out, LOW);
+    digitalWrite(condenser_out, LOW);
+    digitalWrite(throttle_out, LOW);
+    digitalWrite(evaporator_out, LOW);
+    digitalWrite(mass_flow_led, LOW);
+    digitalWrite(capacitance_led, LOW);
+    digitalWrite(power_led, LOW);
+  }
 
-// ================= LCD WRITING AND CANCELLING ===============
-void write_high_pressure_led() {
-  digitalWrite(high_pressure_led, HIGH);
-  digitalWrite(low_pressure_led, LOW);
-  digitalWrite(compressor_out, LOW);
-  digitalWrite(condenser_out, LOW);
-  digitalWrite(throttle_out, LOW);
-  digitalWrite(evaporator_out, LOW);
-  digitalWrite(mass_flow_led, LOW);
-  digitalWrite(efficiency_led, LOW);
-  digitalWrite(power_led, LOW);
-}
+  void write_compressor_out_led() {
+    digitalWrite(high_pressure_led, LOW);
+    digitalWrite(low_pressure_led, LOW);
+    digitalWrite(compressor_out, HIGH);
+    digitalWrite(condenser_out, LOW);
+    digitalWrite(throttle_out, LOW);
+    digitalWrite(evaporator_out, LOW);
+    digitalWrite(mass_flow_led, LOW);
+    digitalWrite(capacitance_led, LOW);
+    digitalWrite(power_led, LOW);
+  }
 
-void write_low_pressure_led() {
-  digitalWrite(high_pressure_led, LOW);
-  digitalWrite(low_pressure_led, HIGH);
-  digitalWrite(compressor_out, LOW);
-  digitalWrite(condenser_out, LOW);
-  digitalWrite(throttle_out, LOW);
-  digitalWrite(evaporator_out, LOW);
-  digitalWrite(mass_flow_led, LOW);
-  digitalWrite(efficiency_led, LOW);
-  digitalWrite(power_led, LOW);
-}
+  void write_condenser_out_led() {
+    digitalWrite(high_pressure_led, LOW);
+    digitalWrite(low_pressure_led, LOW);
+    digitalWrite(compressor_out, LOW);
+    digitalWrite(condenser_out, HIGH);
+    digitalWrite(throttle_out, LOW);
+    digitalWrite(evaporator_out, LOW);
+    digitalWrite(mass_flow_led, LOW);
+    digitalWrite(capacitance_led, LOW);
+    digitalWrite(power_led, LOW);
+  }
 
-void write_compressor_out_led() {
-  digitalWrite(high_pressure_led, LOW);
-  digitalWrite(low_pressure_led, LOW);
-  digitalWrite(compressor_out, HIGH);
-  digitalWrite(condenser_out, LOW);
-  digitalWrite(throttle_out, LOW);
-  digitalWrite(evaporator_out, LOW);
-  digitalWrite(mass_flow_led, LOW);
-  digitalWrite(efficiency_led, LOW);
-  digitalWrite(power_led, LOW);
-}
+  void write_throttle_out_led() {
+    digitalWrite(high_pressure_led, LOW);
+    digitalWrite(low_pressure_led, LOW);
+    digitalWrite(compressor_out, LOW);
+    digitalWrite(condenser_out, LOW);
+    digitalWrite(throttle_out, HIGH);
+    digitalWrite(evaporator_out, LOW);
+    digitalWrite(mass_flow_led, LOW);
+    digitalWrite(capacitance_led, LOW);
+    digitalWrite(power_led, LOW);
+  }
 
-void write_condenser_out_led() {
-  digitalWrite(high_pressure_led, LOW);
-  digitalWrite(low_pressure_led, LOW);
-  digitalWrite(compressor_out, LOW);
-  digitalWrite(condenser_out, HIGH);
-  digitalWrite(throttle_out, LOW);
-  digitalWrite(evaporator_out, LOW);
-  digitalWrite(mass_flow_led, LOW);
-  digitalWrite(efficiency_led, LOW);
-  digitalWrite(power_led, LOW);
-}
+  void write_evaporator_out_led() {
+    digitalWrite(high_pressure_led, LOW);
+    digitalWrite(low_pressure_led, LOW);
+    digitalWrite(compressor_out, LOW);
+    digitalWrite(condenser_out, LOW);
+    digitalWrite(throttle_out, LOW);
+    digitalWrite(evaporator_out, HIGH);
+    digitalWrite(mass_flow_led, LOW);
+    digitalWrite(capacitance_led, LOW);
+    digitalWrite(power_led, LOW);
+  }
 
-void write_throttle_out_led() {
-  digitalWrite(high_pressure_led, LOW);
-  digitalWrite(low_pressure_led, LOW);
-  digitalWrite(compressor_out, LOW);
-  digitalWrite(condenser_out, LOW);
-  digitalWrite(throttle_out, HIGH);
-  digitalWrite(evaporator_out, LOW);
-  digitalWrite(mass_flow_led, LOW);
-  digitalWrite(efficiency_led, LOW);
-  digitalWrite(power_led, LOW);
-}
+  void write_mass_flow_led() {
+    digitalWrite(high_pressure_led, LOW);
+    digitalWrite(low_pressure_led, LOW);
+    digitalWrite(compressor_out, LOW);
+    digitalWrite(condenser_out, LOW);
+    digitalWrite(throttle_out, LOW);
+    digitalWrite(evaporator_out, LOW);
+    digitalWrite(mass_flow_led, HIGH);
+    digitalWrite(capacitance_led, LOW);
+    digitalWrite(power_led, LOW);
+  }
 
-void write_evaporator_out_led() {
-  digitalWrite(high_pressure_led, LOW);
-  digitalWrite(low_pressure_led, LOW);
-  digitalWrite(compressor_out, LOW);
-  digitalWrite(condenser_out, LOW);
-  digitalWrite(throttle_out, LOW);
-  digitalWrite(evaporator_out, HIGH);
-  digitalWrite(mass_flow_led, LOW);
-  digitalWrite(efficiency_led, LOW);
-  digitalWrite(power_led, LOW);
-}
+  void write_capacitance_led() {
+    digitalWrite(high_pressure_led, LOW);
+    digitalWrite(low_pressure_led, LOW);
+    digitalWrite(compressor_out, LOW);
+    digitalWrite(condenser_out, LOW);
+    digitalWrite(throttle_out, LOW);
+    digitalWrite(evaporator_out, LOW);
+    digitalWrite(mass_flow_led, LOW);
+    digitalWrite(capacitance_led, HIGH);
+    digitalWrite(power_led, LOW);
+  }
 
-void write_mass_flow_led() {
-  digitalWrite(high_pressure_led, LOW);
-  digitalWrite(low_pressure_led, LOW);
-  digitalWrite(compressor_out, LOW);
-  digitalWrite(condenser_out, LOW);
-  digitalWrite(throttle_out, LOW);
-  digitalWrite(evaporator_out, LOW);
-  digitalWrite(mass_flow_led, HIGH);
-  digitalWrite(efficiency_led, LOW);
-  digitalWrite(power_led, LOW);
-}
+  void write_power_led() {
+    digitalWrite(high_pressure_led, LOW);
+    digitalWrite(low_pressure_led, LOW);
+    digitalWrite(compressor_out, LOW);
+    digitalWrite(condenser_out, LOW);
+    digitalWrite(throttle_out, LOW);
+    digitalWrite(evaporator_out, LOW);
+    digitalWrite(mass_flow_led, LOW);
+    digitalWrite(capacitance_led, LOW);
+    digitalWrite(power_led, HIGH);
+  }
 
-void write_efficiency_led() {
-  digitalWrite(high_pressure_led, LOW);
-  digitalWrite(low_pressure_led, LOW);
-  digitalWrite(compressor_out, LOW);
-  digitalWrite(condenser_out, LOW);
-  digitalWrite(throttle_out, LOW);
-  digitalWrite(evaporator_out, LOW);
-  digitalWrite(mass_flow_led, LOW);
-  digitalWrite(efficiency_led, HIGH);
-  digitalWrite(power_led, LOW);
-}
+  void blink_greens() {
+    digitalWrite(high_pressure_led, LOW);
+    digitalWrite(low_pressure_led, LOW);
+    digitalWrite(compressor_out, LOW);
+    digitalWrite(condenser_out, LOW);
+    digitalWrite(throttle_out, LOW);
+    digitalWrite(evaporator_out, LOW);
+    digitalWrite(mass_flow_led, LOW);
+    digitalWrite(capacitance_led, LOW);
+    digitalWrite(power_led, LOW);
+    digitalWrite(mass_flow_led, LOW);
+    delay(50);
+    digitalWrite(mass_flow_led, HIGH);
+    delay(50);
+    digitalWrite(capacitance_led, HIGH);
+    delay(50);
+    digitalWrite(power_led, HIGH);
+    delay(50);
+    digitalWrite(mass_flow_led, LOW);
+    digitalWrite(capacitance_led, LOW);
+    digitalWrite(power_led, LOW);
+  }
 
-void write_power_led() {
-  digitalWrite(high_pressure_led, LOW);
-  digitalWrite(low_pressure_led, LOW);
-  digitalWrite(compressor_out, LOW);
-  digitalWrite(condenser_out, LOW);
-  digitalWrite(throttle_out, LOW);
-  digitalWrite(evaporator_out, LOW);
-  digitalWrite(mass_flow_led, LOW);
-  digitalWrite(efficiency_led, LOW);
-  digitalWrite(power_led, HIGH);
-}
-
-void blink_greens() {
-  digitalWrite(high_pressure_led, LOW);
-  digitalWrite(low_pressure_led, LOW);
-  digitalWrite(compressor_out, LOW);
-  digitalWrite(condenser_out, LOW);
-  digitalWrite(throttle_out, LOW);
-  digitalWrite(evaporator_out, LOW);
-  digitalWrite(mass_flow_led, LOW);
-  digitalWrite(efficiency_led, LOW);
-  digitalWrite(power_led, LOW);
-  digitalWrite(mass_flow_led, LOW);
-  delay(50);
-  digitalWrite(mass_flow_led, HIGH);
-  delay(50);
-  digitalWrite(efficiency_led, HIGH);
-  delay(50);
-  digitalWrite(power_led, HIGH);
-  delay(50);
-  digitalWrite(mass_flow_led, LOW);
-  digitalWrite(efficiency_led, LOW);
-  digitalWrite(power_led, LOW);
-}
-
-// ========== TEST CASE FOR BUTTONS AND SWITCH ==============
-void test_buttons() {
-   int t = 50;
-   lcd.clear();
-   lcd.setCursor(0,0);
-   lcd.print("SYSTEM TEST");
-   lcd.setCursor(0,1);
-   lcd.print("HIGH P    ");
-   lcd.print(digitalRead(high_pressure_switch));
-   lcd.setCursor(0,2);
-   lcd.print("LOW P    ");
-   lcd.print(digitalRead(low_pressure_switch));
-   lcd.setCursor(0,3);
-   lcd.print("COMP SWITCH    ");
-   lcd.print(digitalRead(13));
-   write_compressor_out_led();
-   delay(t);
-   write_high_pressure_led();
-   delay(t);
-   write_condenser_out_led();
-   delay(t);
-   write_throttle_out_led();
-   delay(t);
-   write_low_pressure_led();
-   delay(t);
-   write_evaporator_out_led();
-   delay(t);
-   write_mass_flow_led();
-   delay(t);
-   write_power_led();
-   delay(t);
-   write_efficiency_led();
-   delay(t);
-   write_power_led();
-   delay(t);
-   write_mass_flow_led();
-   delay(t);
-   write_evaporator_out_led();
-   delay(t);
-   write_low_pressure_led();
-   delay(t);
-   write_throttle_out_led();
-   delay(t);
-   write_condenser_out_led();
-   delay(t);
-   write_high_pressure_led();
-   delay(t);
-   write_compressor_out_led();
-} 
+  // ========== TEST CASE FOR BUTTONS AND SWITCH ==============
+  void test_buttons() {
+    int t = 50; // miliseconds
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("SYSTEM TEST");
+    lcd.setCursor(0, 1);
+    lcd.print("HIGH P    ");
+    lcd.print(digitalRead(high_pressure_switch));
+    lcd.setCursor(0, 2);
+    lcd.print("LOW P    ");
+    lcd.print(digitalRead(low_pressure_switch));
+    lcd.setCursor(0, 3);
+    lcd.print("COMP SWITCH    ");
+    lcd.print(digitalRead(13));
+    write_compressor_out_led();
+    delay(t);
+    write_high_pressure_led();
+    delay(t);
+    write_condenser_out_led();
+    delay(t);
+    write_throttle_out_led();
+    delay(t);
+    write_low_pressure_led();
+    delay(t);
+    write_evaporator_out_led();
+    delay(t);
+    write_mass_flow_led();
+    delay(t);
+    write_power_led();
+    delay(t);
+    write_capacitance_led();
+    delay(t);
+    write_power_led();
+    delay(t);
+    write_mass_flow_led();
+    delay(t);
+    write_evaporator_out_led();
+    delay(t);
+    write_low_pressure_led();
+    delay(t);
+    write_throttle_out_led();
+    delay(t);
+    write_condenser_out_led();
+    delay(t);
+    write_high_pressure_led();
+    delay(t);
+    write_compressor_out_led();
+  }
--- main.ino
+++ main.ino
@@ -4,10 +4,6 @@
    ===================Yianni , Daniel Howard, Zach Newman ===================
    ==========================================================================
 */
-
-// Liquidcrystal.h  defines the LCD functions and classes.
-// max6675.h defines the K-type thermocouple c
-
 #include <LiquidCrystal.h>
 #include <max6675.h>
 
@@ -21,8 +17,8 @@ const short int ktc5SO = 34, ktc5CS = 36, ktc5CLK = 38;
 const short int ktc6SO = 35, ktc6CS = 37, ktc6CLK = 39;
 const short int ktc7SO = 40, ktc7CS = 42, ktc7CLK = 44;
 const short int ktc8SO = 41, ktc8CS = 43, ktc8CLK = 45;
-
-// Declarations of thermocouples 1-8. 
+ 
+// Declarations of thermocouple objects 1-8.
 MAX6675 ktc1(ktc1CLK, ktc1CS, ktc1SO);
 MAX6675 ktc2(ktc2CLK, ktc2CS, ktc2SO);
 MAX6675 ktc3(ktc3CLK, ktc3CS, ktc3SO);
@@ -32,19 +28,21 @@ MAX6675 ktc6(ktc6CLK, ktc6CS, ktc6SO);
 MAX6675 ktc7(ktc7CLK, ktc7CS, ktc7SO);
 MAX6675 ktc8(ktc8CLK, ktc8CS, ktc8SO);
 
-// Pointers and references for thermocouples. Used to address switch mode feature of compressors. 
+// Pointers and references for thermocouples. Used to address switch mode feature of compressors.
 MAX6675 *ptrktc1 = &ktc1;
-MAX6675 *ptrktc2 = &ktc2; 
-MAX6675 *ptrktc3 = &ktc3; 
+MAX6675 *ptrktc2 = &ktc2;
+MAX6675 *ptrktc3 = &ktc3;
 MAX6675 *ptrktc4 = &ktc4;
 MAX6675 *ptrktc5 = &ktc5;
 MAX6675 *ptrktc6 = &ktc6;
 MAX6675 *ptrktc7 = &ktc7;
 MAX6675 *ptrktc8 = &ktc8;
 
-// Address by 0/1 row (scroll vs compressor) at the 1-4 index. 
-MAX6675 ptrKtc[2][4] = {{*ptrktc1, *ptrktc2, *ptrktc3, *ptrktc4},
-                          {*ptrktc5, *ptrktc6, *ptrktc7, *ptrktc8}};
+// Address by 0/1 row (scroll vs compressor) at the 0-3 index.
+MAX6675 ptrKtc[2][4] = {
+  {*ptrktc1, *ptrktc2, *ptrktc3, *ptrktc4},
+  {*ptrktc5, *ptrktc6, *ptrktc7, *ptrktc8}
+};
 
 // ========== LCD SECTION ============
 // Pin declarations and instance creation.
@@ -64,7 +62,7 @@ const short int throttle_out = 2;
 
 // LED pins for performance
 const short int mass_flow_led = 8;
-const short int efficiency_led = 9;
+const short int capacitance_led = 9;
 const short int power_led = 10;
 
 // ========== BUTTON INPUTS ==========
@@ -75,15 +73,9 @@ const short int high_pressure_switch = 12, low_pressure_switch = 11;
 short int comp_mode_switch = 13;
 
 // ========== CALCULATION VALUES ===========
-// Variables for the interface.
+// Variables for the interface and switch mode. 
 int high_side_pressure = 0;
 int low_side_pressure = 0;
-
-// Derived values for the cycle.
-double mass_flow = 0;
-double capacity = 0;
-double power = 0;
-int efficiency = 0;
 
 // ============= COMPRESSOR MODE ===============
 // 2D array for refrigeration modes
@@ -102,7 +94,7 @@ void setup() {
   pinMode(throttle_out, OUTPUT);
   pinMode(evaporator_out, OUTPUT);
   pinMode(mass_flow_led, OUTPUT);
-  pinMode(efficiency_led, OUTPUT);
+  pinMode(capacitance_led, OUTPUT);
   pinMode(power_led, OUTPUT);
 
   // ======= BUTTON & SWITCH INPUTS ==========
@@ -112,92 +104,131 @@ void setup() {
 }
 
 void loop() {
-  // Check the potentiometer for rotary input.
-  int main_resistance = analogRead(A0);
-  Serial.println(main_resistance);
+/*
+ * 
+ * 
+ * How the loop works: all dependencies and methods. 
+ */
 
+  // Check the potentiometer for resistance input (0-1024)
+  int main_resistance = analogRead(A0);
+ 
   // ============== MENU LOOP ==================
-  // Check the status of the high pressure killswitch. If low, open the menu.
+  // Check status of high pressure toggle switch. If high, open the menu.
   if (digitalRead(high_pressure_switch) == HIGH) {
     high_pressure_menu();
   }
 
-  // Check the status of the low pressure killswitch. If low, open the menu.
+  // Check status of low pressure toggle switch. If high, open the menu.
   if (digitalRead(low_pressure_switch) == HIGH) {
     low_pressure_menu();
   }
 
-
   // ============ MAIN LED LOOP ================
   // First potentiometer knob. Displays compressor mode and compressor outlet temperature.
-  if (main_resistance < 5) {
+  if (main_resistance < 10) {
     write_compressor_out_led();
-    write_lcd("Compressor Temp", ptrKtc[digitalRead(comp_mode_switch)][0].readCelsius());
-    lcd.print("C");
+    write_lcd("Compressor Temp", ptrKtc[digitalRead(comp_mode_switch)][0].readFahrenheit());
+    lcd.print(" F");
   }
 
+  // Displays high side pressure value, 0 if not input, and blue leds.
   else if (main_resistance < 45) {
     write_high_pressure_led();
     write_lcd("High Side P", high_side_pressure);
   }
 
+  // Displays condenser outlet temerature and lights condenser out LED.
   else if (main_resistance < 160) {
     write_condenser_out_led();
-    write_lcd("Condenser Out",  ptrKtc[digitalRead(comp_mode_switch)][1].readCelsius());
-    lcd.print("C");
+    write_lcd("Condenser Out",  ptrKtc[digitalRead(comp_mode_switch)][1].readFahrenheit());
+    lcd.print(" F");
   }
 
+  // Displays metering device out temp and led
   else if (main_resistance < 270) {
     write_throttle_out_led();
-    write_lcd("Throttle Out",  ptrKtc[digitalRead(comp_mode_switch)][2].readCelsius());
-    lcd.print("C");
+    write_lcd("Throttle Out",  ptrKtc[digitalRead(comp_mode_switch)][2].readFahrenheit());
+    lcd.print(" F");
   }
 
+  // Lights low side pressure leds and displays, 0 if not entered, low side pressure.
   else if (main_resistance < 400) {
     write_low_pressure_led();
     write_lcd("Low Side P", low_side_pressure);
-    lcd.print("kPa");
+    lcd.print(" kPa");
   }
 
+  // Lights evaporator out LED, displays thermocouple temperature.
   else if (main_resistance < 515) {
     write_evaporator_out_led();
-    write_lcd("Evaporator Out",  ptrKtc[digitalRead(comp_mode_switch)][3].readCelsius());
+    write_lcd("Evaporator Out",  ptrKtc[digitalRead(comp_mode_switch)][3].readFahrenheit());
+    lcd.print(" F");
   }
 
+  // Writes MASS FLOW out and turns on first green light.
   else if (main_resistance < 635) {
+    float MF_R[10] = {59.33840856, 1.006688227, 0.2030794489, 0.01488044391, 0.01294057197,
+                  -0.00353467516, 4.25E-05, 3.13E-05, -6.69E-05, 8.47E-06 };
+    float MF_SC[10] = {40.15698882, 1.096371113, 0.14024549, 0.015865756, 0.002097207,
+                   -0.001690066, 9.03205E-05, -2.70756E-05, -1.35767E-05, 5.15408E-06 };
+    float mass_flow_values[2] = {
+      calculate(MF_R, ptrKtc[digitalRead(comp_mode_switch)][1].readFahrenheit(), ptrKtc[digitalRead(comp_mode_switch)][3].readFahrenheit()), 
+      calculate(MF_SC, ptrKtc[digitalRead(comp_mode_switch)][1].readFahrenheit(),ptrKtc[digitalRead(comp_mode_switch)][3].readFahrenheit()) 
+      };
     write_mass_flow_led();
-    mass_flow = calculate_mass_flow(ptrKtc[digitalRead(comp_mode_switch)][3].readCelsius(),
-                                       ptrKtc[digitalRead(comp_mode_switch)][1].readCelsius());
-    write_lcd("Mass flow", mass_flow);
-    lcd.print("kg/s");
+    write_lcd("Mass Flow:", mass_flow_values[ digitalRead(comp_mode_switch)] );
+    lcd.print(" lbs/hr");
   }
 
+  // Writes refrigerant CAPACITANCE out and lights second green led.
   else if (main_resistance < 760) {
-    write_efficiency_led();
-    capacity = calculate_capacity(ptrKtc[digitalRead(comp_mode_switch)][3].readCelsius(),
-                                       ptrKtc[digitalRead(comp_mode_switch)][1].readCelsius());
-    // Named efficiency right now: Will change to capacitance, or whatever else down the road. 
-    write_lcd("Capacitance:", capacity );
+    const float CAP_R[10] = {5897.679, 112.7528, 9.887627, 1.777997, 0.5887399, -0.4782697,
+                   7.58E-04, -0.005700553, -0.00568108, 0.001614988 };
+    const float CAP_SC[10] = {4433.99929, 107.9502108, -6.654767819, 2.002620595, -0.2210224213,
+                    -0.1026412673, 0.002942954154, -0.00968481191, -0.0006961641807, 0.0002807495071 };
+    write_capacitance_led();  
+    float capacitance_values[2] = {
+      calculate(CAP_R, ptrKtc[digitalRead(comp_mode_switch)][1].readFahrenheit(), ptrKtc[digitalRead(comp_mode_switch)][3].readFahrenheit()), 
+      calculate(CAP_SC, ptrKtc[digitalRead(comp_mode_switch)][1].readFahrenheit(),ptrKtc[digitalRead(comp_mode_switch)][3].readFahrenheit()) 
+      };
+    // Named efficiency right now: Will change to capacitance, or whatever else down the road.
+    write_lcd("Capacitance:", capacitance_values[digitalRead(comp_mode_switch)]);
+    lcd.print(" BTU/hr");
   }
 
+  // Writes POWER output to LCD and lights third green LED.
   else if (main_resistance < 880) {
     write_power_led();
-    power = calculate_power(ptrKtc[digitalRead(comp_mode_switch)][3].readCelsius(),
-                                       ptrKtc[digitalRead(comp_mode_switch)][1].readCelsius());
-    write_lcd("Power", power);
-    lcd.print("KW");
+    const float W_R[10] = {126.9581, -5.372865, 8.524027, -0.09801806, 0.1522044, -0.05396478,
+                 -9.06E-04, 9.33E-04, -2.39E-04, 1.16E-04
+                };
+    const float W_SC[10] = {645.1275928, -2.295127832, -7.218567883, -0.005535838, 0.042861644,
+                  0.076252192, 0.000563255, -0.000610193, 0.000112464, -0.000130886
+                 };           
+    float power_values[2] = {
+      calculate(W_R, ptrKtc[digitalRead(comp_mode_switch)][1].readFahrenheit(), ptrKtc[digitalRead(comp_mode_switch)][3].readFahrenheit()), 
+      calculate(W_SC, ptrKtc[digitalRead(comp_mode_switch)][1].readFahrenheit(),ptrKtc[digitalRead(comp_mode_switch)][3].readFahrenheit())
+      };
+    write_lcd("Power: ", power_values[digitalRead(comp_mode_switch)]);
+    lcd.print(" Watts");
   }
 
+  // Writes current draw out and flashes green leds. (Possibly efficiency);
   else if (main_resistance < 990) {
-   efficiency = 234;
-   blink_greens();
-   write_lcd("Efficiency", efficiency);
+    blink_greens();
+    const float A_R[10] = {3.652199, -0.0372356, 0.04894778, -8.07E-04, 9.86E-04, -2.83E-04,
+                 -1.06E-05, 1.10E-05, -1.32E-06, 5.34E-07 };
+    const float A_SC[10] = {2.558134799, 0.001949875929, -0.000379452699, -0.00009545899939, -0.00003352478468,
+                  0.00007065422042, 0.000004294488347, -0.000003643615934, 0.000001638983358, -0.00000004068571839 };
+    float current_values[2] = {calculate(A_R, 100,100), calculate(A_SC, 100,100)};
+    write_lcd("Current ", current_values[digitalRead(comp_mode_switch)]);
   }
 
   else if (main_resistance < 1024) {
-   while (analogRead(A0) > 1010) {
-    test_buttons();
-   }
+    while (analogRead(A0) > 1010) {
+      test_buttons();
+    }
   }
   delay(450);
 }
